ca9e8465f08a04faad8618362c3762dd3ecef6da
Måns Alklint <mans@klintan.net>
2024-10-21 01:38:51 +0200

    Refactor: Update MovieCard grid layout

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ export const MovieCard = ({ movie, handleShowModal }) => {
   const hasPoster = movie.poster_path !== null;
 
   return (
-    <div key={movie.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+    <div key={movie.id} className="col-12 col-sm-6 col-md-4 col-lg-2 mb-4">
       <div className={"card h-100"}>
         {hasPoster ? (
           <img
