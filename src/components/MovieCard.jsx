6873cfaf96e411045a0fe5d5d800926f5d309846
Måns Alklint <mans@klintan.net>
2024-10-21 01:09:43 +0200

    Refactor: Update MovieCard style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,28 +1,23 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import styles from "../styles/MovieCard.module.css";
 
 export const MovieCard = ({ movie, handleShowModal }) => {
   return (
-    <div
-      key={movie.id}
-      className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
-      style={{ width: "300px", height: "auto" }}
-    >
-      <div className="card h-100">
+    <div key={movie.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+      <div className={"card h-100"}>
         <img
           src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
           alt={movie.title}
           className="card-img-top"
         />
-        <div className="card-body">
-          <h5 className="card-title">{movie.title}</h5>
-          <Button
-            variant="primary"
-            className="mt-3"
-            onClick={() => handleShowModal(movie)}
-          >
-            Write a Review
-          </Button>
+        <div className={styles.cardBody}>
+          <h5 className={styles.cardTitle}>{movie.title}</h5>
+          <div className={styles.buttonWrap}>
+            <Button variant="primary" onClick={() => handleShowModal(movie)}>
+              Write a Review
+            </Button>
+          </div>
         </div>
       </div>
     </div>
