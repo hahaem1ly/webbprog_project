1b8fdc8e1ad656af48e123a8a63fd12130aea2c3
Måns Alklint <mans@klintan.net>
2024-10-21 01:25:42 +0200

    Refactor: Update NowPlaying component to use MovieCard

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -1,60 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { fetchNowPlaying} from '../services/movieService'; // Import the API service
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import { fetchNowPlaying } from "../services/movieService"; // Import the API service
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MovieCard } from "./MovieCard";
 
-const NowPlaying = () =>{
-    const[nowPlaying, setNowPlaying] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+const NowPlaying = () => {
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-
-    useEffect(() => {
-        const getMovies = async () => {
-            try {
-                const response = await fetchNowPlaying();
-                setNowPlaying(response.data.results);
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching upcoming movies:', error);
-                setError('Failed to load movies.');
-                setLoading(false);
-              }
-        };
-       getMovies(); 
-    }, []);
-
-    if (loading) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const response = await fetchNowPlaying();
+        setNowPlaying(response.data.results);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching upcoming movies:", error);
+        setError("Failed to load movies.");
+        setLoading(false);
       }
-    
-      if (error) {
-        return <div>{error}</div>;
-      }
-
-      return (
-        <div>
-          <h1>Now Playing</h1>
-          <div className="row">
-            {nowPlaying.map((movie) => (
-              <div key={movie.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" style={{ width: '300px', height: 'auto' }}>
-                <div className="card h-100">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                    alt={movie.title}
-                    className="card-img-top"
-                  />
-                   <div className="card-body">
-                    <h3 className="card-title">{movie.title}</h3>
-                    <h5>Release date: {movie.release_date}</h5>
-                    </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      );
     };
-    
+    getMovies();
+  }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Now Playing</h1>
+      <div className="row">
+        {nowPlaying.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default NowPlaying;
